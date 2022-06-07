7150b10393565153dd19ec28f9dd9e040cdb7af1
Ignat Polnyakov <ignat-88@bk.ru>
2022-06-07 13:58:01 +0300

    new function

--- js/main.js
+++ js/main.js
@@ -1,18 +1,12 @@
-function validateLength(value, max) {
-  if(value){
-    let len = value.length;
-    if (len >= max){
-      print 'длинна строки должна быть не больше' +max ;
-    }
-
+function validateLength(value, max){
+  return value.length <= max
   }
-  return null
-}
 validateLength('text', 140);
 
 function getRandomInt(min, max) {
-min = Math.ceil(min);
-max = Math.floor(max);
+
+  min = Math.ceil(min);
+  max = Math.floor(max);
 return Math.floor(Math.random()*(max-min+1))+min;
 }
 
