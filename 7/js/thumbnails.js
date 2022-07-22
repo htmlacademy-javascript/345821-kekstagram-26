7b8a5cc559099f40feda18180549223928a3cd46
Keks <keks@htmlacademy.ru>
2022-07-22 23:30:34 +0300

    :heavy_check_mark: Сборка #7

--- 7/js/thumbnails.js
+++ 7/js/thumbnails.js
@@ -9,12 +9,12 @@ const createPhotos = userPhoto(25);
 const photoListFragment = document.createDocumentFragment();
 
 const makePhotos = () => {
-  createPhotos.forEach(({url, likes, comments}) => {
+  createPhotos.forEach((picture) => {
     const photoElement = photoTemplate.cloneNode(true);
-    photoElement.querySelector('.picture__img').src = url;
-    photoElement.querySelector('.picture__likes').textContent = likes;
-    photoElement.querySelector('.picture__comments').textContent = comments.length;
-    photoElement.addEventListener('click', () => bigPictureFullscreen);
+    photoElement.querySelector('.picture__img').src = picture.url;
+    photoElement.querySelector('.picture__likes').textContent = picture.likes;
+    photoElement.querySelector('.picture__comments').textContent = picture.comments.length;
+    photoElement.addEventListener('click', () => bigPictureFullscreen(picture));
     photoListFragment.appendChild(photoElement);
   });
   photoListElement.appendChild(photoListFragment);
