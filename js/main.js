417f1656e0486137f936a64c508f6b9618f078a3
Ignat Polnyakov <ignat-88@bk.ru>
2022-07-30 20:25:09 +0300

    Final

--- js/main.js
+++ js/main.js
@@ -1,7 +1,13 @@
 import {makePhotos} from './thumbnails.js';
 import {uploadPhotoForm} from './upload-form.js';
+import {getData} from './api.js';
+import {createErrorMessage} from './popups.js';
+import {drawFilteredPhotos} from './photo-filter.js';
 
 
-makePhotos();
 uploadPhotoForm();
+getData((photos) => {
+  makePhotos(photos);
+  drawFilteredPhotos(photos);
+}, createErrorMessage);
 
--- js/thumbnails.js
+++ js/thumbnails.js
@@ -1,15 +1,22 @@
-import {userPhoto} from './data.js';
 import {bigPictureFullscreen} from './bigpicture.js';
 
 const photoListElement = document.querySelector('.pictures');
 const photoTemplate = document.querySelector('#picture').content.querySelector('.picture');
+const imgFilters = document.querySelector('.img-filters');
 
-const createPhotos = userPhoto(25);
 
 const photoListFragment = document.createDocumentFragment();
 
-const makePhotos = () => {
-  createPhotos.forEach((picture) => {
+const clearPicturesList = () => {
+  const pictures = document.querySelectorAll('.picture');
+  pictures.forEach((element) => element.remove());
+};
+
+const makePhotos = (pictures) => {
+  if (imgFilters.classList.contains('img-filters--inactive')) {
+    imgFilters.classList.remove('img-filters--inactive');
+  }
+  pictures.forEach((picture) => {
     const photoElement = photoTemplate.cloneNode(true);
     photoElement.querySelector('.picture__img').src = picture.url;
     photoElement.querySelector('.picture__likes').textContent = picture.likes;
@@ -17,6 +24,7 @@ const makePhotos = () => {
     photoElement.addEventListener('click', () => bigPictureFullscreen(picture));
     photoListFragment.appendChild(photoElement);
   });
+  clearPicturesList();
   photoListElement.appendChild(photoListFragment);
 };
 
